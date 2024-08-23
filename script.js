ae4515b9ee9ac4ca0c7690dfc447d41ada7cf920
ella-aaa <ells@sooho.io>
2024-08-23 12:34:24 +0900

    prettier 설정 추가

--- script.js
+++ script.js
@@ -2,7 +2,35 @@ import {Lum0x} from "lum0x-sdk";
 
 Lum0x.init('TEST_KEY');
 
+//채널 정보(팔로워)
 async function getChannelBulkSearch() {
+    const data = await Lum0x.farcasterChannel.getChannelBulkSearch({
+        ids: "degenpad,farcaster-rollup",
+        type: "id"
+    });
+    console.log(data);
+}
+
+//유저 정보(팔로워)
+async function getUserByFids() {
+    const data = await Lum0x.farcasterChannel.getChannelBulkSearch({
+        ids: "degenpad,farcaster-rollup",
+        type: "id"
+    });
+    console.log(data);
+}
+
+//특정 유저가 작성한 Cast
+async function getCastsByFid() {
+    const data = await Lum0x.farcasterChannel.getChannelBulkSearch({
+        ids: "degenpad,farcaster-rollup",
+        type: "id"
+    });
+    console.log(data);
+}
+
+//특정 Cast의 리액션 정보
+async function getCastReaction() {
     const data = await Lum0x.farcasterChannel.getChannelBulkSearch({
         ids: "degenpad,farcaster-rollup",
         type: "id"
